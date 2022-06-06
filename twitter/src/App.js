ecd9e6674a3eb9c51b9a399dd77e74f3aef802bc
SREEVATSAN1252 <sreevatsan1252@gmail.com>
2022-06-06 19:57:31 +0530

    latest changes

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,21 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hey yoo. lets goo</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      hello lets build twitter🐱
+      <Sidebar />
     </div>
   );
 }
